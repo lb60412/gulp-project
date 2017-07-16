9391845681903f4ebee85a717abd1a2efc0fd97e
吕斌 <bin.lv@moji.com>
2017-07-16 11:58:50 +0800

    add babel-runtime && update presets

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,24 @@
 'use strict';
-/* global Redux */
+
+const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10,11];
+
+
+const backData = async(num) => {
+    return new Promise((resolve, reject) => {
+        setInterval(() => {
+            resolve(num)
+        }, 3000);
+    });
+}
+
+
+const getData = async() => {
+    let i = 0;
+    while (arr[i]) {
+        let num = await backData(i);
+        console.log(num);
+        i++;
+    }
+}
+
+getData();
--- webpack.config.js
+++ webpack.config.js
@@ -1,31 +1,38 @@
 import webpack from 'webpack';
 
-export default {
-	entry: {
-		index: './src/js/index.js'
-	},
-	output: {
-		path: '/dist',
-		filename: '[name].js',
-		publicPath: '/dist/'
-	},
-	module: {
-		loaders: [{
-			test: /\.js/,
-			loader: 'babel-loader'
-		}]
-	},
-	externals: {
-		'react': 'React',
-		'react-dom': 'ReactDOM',
-		'Zepto': 'Zepto',
-		'anim': 'anim'
-	},
-	plugins: [
-		new webpack.optimize.UglifyJsPlugin({
-			compress: {
-				warnings: false
-			}
-		})
-	]
+const defaultConfig = {
+    entry: {
+        index: './src/js/index.js'
+    },
+    output: {
+        path: '/dist',
+        filename: '[name].js',
+        publicPath: '/dist/'
+    },
+    module: {
+        loaders: [{
+            test: /\.js/,
+            loader: 'babel-loader'
+        }]
+    },
+    externals: {
+        'react': 'React',
+        'react-dom': 'ReactDOM',
+        'Zepto': 'Zepto',
+        'anim': 'anim'
+    }
+};
+
+const pluginsConfig = {
+    plugins: [
+        new webpack.optimize.UglifyJsPlugin({
+            compress: {
+                warnings: false
+            }
+        })
+    ]
 }
+
+export var develop = defaultConfig;
+
+export var publish = Object.assign({}, defaultConfig, pluginsConfig);
